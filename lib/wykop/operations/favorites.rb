d253fad0a8db34269d67ca7bcf2d5fd3f2e999d1
Lukasz Raczylo <lukasz@raczylo.com>
2014-11-18 00:39:42 +0000

    moved replace_url to @request

--- lib/wykop/operations/favorites.rb
+++ lib/wykop/operations/favorites.rb
@@ -7,42 +7,27 @@ module Wykop
       end
 
       def favorite_index( p = {} )
-        q_url = replace_url({ :banana => 'favorites', :potato => 'index', :param1 => p[:param1] } )
+        q_url = @request.replace_url({ :banana => 'favorites', :potato => 'index', :param1 => p[:param1] } )
         q_body = Hash.new
         return @request.execute(q_url, q_body)
       end
 
       def favorite_comments( p = {} )
-        q_url = replace_url({ :banana => 'favorites', :potato => 'comments' } )
+        q_url = @request.replace_url({ :banana => 'favorites', :potato => 'comments' } )
         q_body = Hash.new
         return @request.execute(q_url, q_body)
       end
 
       def favorite_entries( p = {} )
-        q_url = replace_url({ :banana => 'favorites', :potato => 'entries' } )
+        q_url = @request.replace_url({ :banana => 'favorites', :potato => 'entries' } )
         q_body = Hash.new
         return @request.execute(q_url, q_body)
       end
 
       def favorite_lists( p = {} )
-        q_url = replace_url({ :banana => 'favorites', :potato => 'lists' } )
+        q_url = @request.replace_url({ :banana => 'favorites', :potato => 'lists' } )
         q_body = Hash.new
         return @request.execute(q_url, q_body)
-      end
-
-      def replace_url( p = {} )
-        standard_url = "#{@client.configuration.api_host}/banana/potato/param1/param2/appkey,#{@client.configuration.app_user_key}/userkey,#{@client.user_info['userkey']}"
-        if ! p[:param1].nil?
-          standard_url = standard_url.gsub(/param1/, p[:param1].to_s)
-        else
-          standard_url = standard_url.gsub(/\/param1/, '')
-        end
-        if ! p[:param2].nil?
-          standard_url = standard_url.gsub(/param2/, p[:param2].to_s)
-        else
-          standard_url = standard_url.gsub(/\/param2/, '')
-        end
-        return standard_url.gsub(/banana/, p[:banana]).gsub(/potato/, p[:potato])
       end
     end
   end
--- lib/wykop/operations/tags.rb
+++ lib/wykop/operations/tags.rb
@@ -7,14 +7,9 @@ module Wykop
       end
 
       def tags_show
-        q_url = replace_url({ :banana => 'tags', :potato => 'index' })
+        q_url = @request.replace_url({ :banana => 'tags', :potato => 'index' })
         q_body = Hash.new
         return @request.execute(q_url, q_body)
-      end
-
-      def replace_url( p = {} )
-        standard_url = "#{@client.configuration.api_host}/banana/potato/appkey,#{@client.configuration.app_user_key}/userkey,#{@client.user_info['userkey']}"
-        return standard_url.gsub(/banana/, p[:banana]).gsub(/potato/, p[:potato])
       end
     end
   end
